805efff426e4b7d0f9c878783411eaec4c17b8f1
Steve Tauber <taubers@gmail.com>
2021-01-02 12:01:44 +0100

    Add belongs to relationship

--- app/Exchanges/Exchange.php
+++ app/Exchanges/Exchange.php
@@ -2,10 +2,12 @@
 
 namespace App\Exchanges;
 
+use App\Teams\Team;
 use Database\Factories\ExchangeFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Exchange extends Model
 {
@@ -19,6 +21,11 @@ class Exchange extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 
     protected static function newFactory(): Factory
     {
