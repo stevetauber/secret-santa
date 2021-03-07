aa3e3d32113d3b1e5dbc6617afc407354283a8a5
Steve Tauber <taubers@gmail.com>
2021-03-07 11:16:14 +0100

    Add livewire exchange component

---
+++ app/Http/Livewire/Exchange/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Exchange;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('exchanges.show');
+    }
+}
